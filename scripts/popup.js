4f5fb7bf0584e468da693adecdcb667a2ea51d79
Abhishek Das <Abhishek Das>
2017-06-06 01:38:10 +0530

    Added feature to lookup timestamps of images and iframes

--- scripts/popup.js
+++ scripts/popup.js
@@ -24,6 +24,13 @@ function view_all_function(){
 }
 
 
+
+
 document.getElementById('save_now').onclick = save_now_function;
 document.getElementById('recent_capture').onclick = recent_capture_function;
 document.getElementById('first_capture').onclick = first_capture_function;
+
+
+
+
+
